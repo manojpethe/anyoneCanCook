a0bf30694c25cd5634a33ec16a84ae26d7c89776
Manoj Pethe <manoj.pethe@gmail.com>
2024-09-26 13:11:31 +0530

    syntax ?. updated

--- lets-cook/pages/courses/course-item/index.tsx
+++ lets-cook/pages/courses/course-item/index.tsx
@@ -13,15 +13,16 @@ type CourseItemProps = {
 
 const CourseItem: React.FC<CourseItemProps> = ({data}) => {
   const router = useRouter()
+  console.log(JSON.stringify(data));
 
   const visitCourse = (id:number)=>{
     router.push("/courses/" + id)
   }
 
   return (
-    <div key={data.id} className="bg-black-200">
+    <div key={data?.id} className="bg-black-200">
         <div style={{height:"90%"}}>
-            <img src={data.imgPath} />
+            <img src={data?.imgPath} />
         </div>
         <div className='flex justify-center items-center'>
         <button onClick={()=>{visitCourse(data.id)}} className='bg-red-500 text-white rounded-full border-2 border-white px-4 py-2 hover:bg-green-500'>
